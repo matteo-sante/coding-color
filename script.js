30e8daca37d172b1a160cb5933bfbf8db7efae74
matteo-sante <matteosantevecchi@gmail.com>
2026-01-14 09:48:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
 console.log("Inizio esecuzione");
-let d = new Date();
-console.log(d);
-document.getElementById("saluto").innerHTML = d;
-let colori = [];
-let i = 0; // conta il numero dei numeri inseriti 
 
-function elabora(){
-    let a = document.getElementById("inNumber").value;
-    alert(a);
-    //se compreso tra 0 e 255
-    if (a>=0 && a<=255){
-        //inserire il numero dentro un array di colori
-        colori.push(a);
-        //visualizzare il numero dentro il div output
-        document.getElementById("outText").innerHTML = "Hai inserito il numero " + a;
-        //visuasualizzare i numeri inseriti del colore scelto
-        console.log(colori);
-        document.getElementById("outText").innerHTML += "<br> I numeri inseriti" + colori;
-        i++;
-        if(i == 3){
-            NewColor = `rgb(${colori[0]},${colori[1]},${colori[2]})`;
-            document.getElementById("outText").style.color=NewColor;
-            return;
-        }
-    }
+let d = new Date();
+document.getElementById("saluto").innerHTML = "Oggi è " + d;
+
+let colori = [];
+
+function elabora() {
+let numero = document.getElementById("inNumber").value;
+let testo = document.getElementById("inNumber2").value;
+
+console.log(numero);
+console.log(testo);
+
+if (numero < 0 || numero > 255 || numero === "") {
+document.getElementById("error").innerHTML = "Il valore inserito non è corretto (0-255)";
+return;
+} else {
+document.getElementById("error").innerHTML = "";
 }
-function annulla(){
-    document.getElementById("inNumber").value = " ";
-    document.getElementById("inNumber2").value = " ";
-    document.getElementById("outText").innerHTML = " ";
-    alert(colori);
+
+colori.push(numero);
+console.log(colori);
+
+document.getElementById("outText").innerHTML =
+"Valori RGB inseriti: " + colori.join(", ");
+
+if (colori.length === 3) {
+let newColor = `rgb(${colori[0]}, ${colori[1]}, ${colori[2]})`;
+document.getElementById("outText").innerHTML = testo;
+document.getElementById("outText").style.color = newColor;
+}
+}
+
+function annulla() {
+document.getElementById("inNumber").value = "";
+document.getElementById("inNumber2").value = "";
+document.getElementById("outText").innerHTML = "";
+document.getElementById("error").innerHTML = "";
+colori = [];
 }
